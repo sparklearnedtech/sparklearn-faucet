d80b4d039e8021aafc806c98f77820d0e766056e
Harvey Javier <harveyjavier17@gmail.com>
2022-05-13 22:10:36 +0800

    Comment remaining `console.log()`s and uncomment error logs

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
   componentWillMount() {
     window.web3 = configureWeb3(`https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_AK}`);
     window.ethTx = require("ethereumjs-tx").Transaction;
-    console.log(window.web3);
+    //console.log(window.web3);
     this.initAndFetchBal([{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}], "0x962D8c38D4088F3F62e70740df46dB214718a855");
     this.fetchGasPrice();
   }
@@ -40,7 +40,7 @@ class App extends React.Component {
   async initAndFetchBal(abi, address) {
     window.token = await new window.web3.eth.Contract(abi, address);
     this.fetchTokenBal("0xca84b6581d325e6a497d875c4ca093ac3ba2ccaf");
-    console.log(window.token);
+    //console.log(window.token);
   }
 
   async fetchTokenBal(address) {
@@ -80,7 +80,7 @@ class App extends React.Component {
         this.estimateGasLimit("0xca84b6581d325e6a497d875c4ca093ac3ba2ccaf", this.state.input_address, "10").then((g) => {
           window.web3.eth.getTransactionCount("0xca84b6581d325e6a497d875c4ca093ac3ba2ccaf", "pending", (error, txCount) => {
             if (error){
-              //console.log(error);
+              console.log(error);
               this.setState({ is_error: true, error_message: "Something has occurred. Please try again." });
             } else {
               // prepare tx
@@ -99,7 +99,7 @@ class App extends React.Component {
               //console.log(tx);
               window.web3.eth.sendSignedTransaction(`0x${tx.serialize().toString("hex")}`, (err, receipt) => {
                 if (err){
-                  //console.log(err);
+                  console.log(err);
                   this.setState({ is_error: true, error_message: "Something has occurred. Please try again." });
                 } else{
                   //console.log(receipt.toString());
@@ -108,7 +108,7 @@ class App extends React.Component {
               });
             }
           }).catch((error) => {
-            //console.log(error);
+            console.log(error);
             this.setState({ is_error: true, error_message: "Something has occurred. Please try again." });
           });
         });
