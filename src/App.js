096c3ceac38689e4dc90ceeca1f3301ee2764eea
Harvey Javier <harveyjavier17@gmail.com>
2022-10-11 22:13:03 +0800

    Transfer contents from Ropsten tab to Goerli, and switch the API as well

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    window.web3 = configureWeb3(`https://ropsten.infura.io/v3/${process.env.REACT_APP_INFURA_AK}`);
+    window.web3 = configureWeb3(`https://goerli.infura.io/v3/${process.env.REACT_APP_INFURA_AK}`);
     window.ethTx = require("ethereumjs-tx").Transaction;
     //console.log(window.web3);
-    this.initAndFetchBal([{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}], "0x962D8c38D4088F3F62e70740df46dB214718a855");
+    this.initAndFetchBal([{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_value","type":"uint256"}],"name":"burn","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"},{"name":"data","type":"bytes"}],"name":"approveAndCall","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[],"name":"newOwner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"tokenAddress","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"_newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Burn","type":"event"}], "0xb8080B8B6BAffadb5DC50f9D31Dc126f2b99c7aA");
     this.fetchGasPrice();
   }
 
@@ -96,7 +96,7 @@ class App extends React.Component {
               //console.log(rawTx);
 
               // build tx
-              const tx = new window.ethTx(rawTx, { chain: "ropsten" });
+              const tx = new window.ethTx(rawTx, { chain: "goerli" });
               tx.sign(privateKey);
               //console.log(tx);
               window.web3.eth.sendSignedTransaction(`0x${tx.serialize().toString("hex")}`, (err, receipt) => {
@@ -105,7 +105,7 @@ class App extends React.Component {
                   this.setState({ is_error: true, error_message: "Something has occurred. Please try again." });
                 } else{
                   //console.log(receipt.toString());
-                  this.setState({ input_address: "", is_success: true, success_hash: receipt.toString(), success_message: "Sending 10 rSRK. Please wait for a few minutes." });
+                  this.setState({ input_address: "", is_success: true, success_hash: receipt.toString(), success_message: "Sending 10 test SRK. Please wait for a few minutes." });
                 }
               });
             }
@@ -133,13 +133,26 @@ class App extends React.Component {
             <div className="card">
               <div className="card-header text-center">
                 <img alt="logo" className="align-self-center" src={logo} height="69" width="80" />
-                <h3>SparkLearn EdTech rSRK Faucet</h3>
+                <h3>SparkLearn EdTech Test SRK Faucet</h3>
               </div>
               <div className="card-body">
-                <Tabs defaultActiveKey="first">
+                <Tabs defaultActiveKey="second">
                   <Tab eventKey="first" title="ETH Ropsten">
+                    <div className="container">
+                      <div className="row">
+                        <div className="col-12">
+                          <br/>
+                          <div key={1} className="alert alert-danger">
+                            <strong>Warning!</strong><br/>
+                            Ropsten is shutting down Q4 of 2022. More info <a href="https://blog.ethereum.org/2022/06/21/testnet-deprecation/" target="_blank" rel="noreferrer">here</a>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </Tab>
+                  <Tab eventKey="second" title="ETH Goerli">
                     <br/>
-                    <center><small>Remaining faucet balance: {this.state.balance} rSRK</small></center><br/>
+                    <center><small>Remaining faucet balance: {this.state.balance} test SRK</small></center><br/>
                     <form>
                       <div className="container">
                         <div className="row">
@@ -152,7 +165,7 @@ class App extends React.Component {
                                 <br key={0} />,
                                 <div key={1} className="alert alert-success">
                                   <strong>Success!</strong>{ " " + this.state.success_message }
-                                  <br/><a href={"https://ropsten.etherscan.io/tx/" + this.state.success_hash} target="_blank" rel="noreferrer">{ "https://ropsten.etherscan.io/tx/" + this.state.success_hash }</a>
+                                  <br/><a href={"https://goerli.etherscan.io/tx/" + this.state.success_hash} target="_blank" rel="noreferrer">{ "https://goerli.etherscan.io/tx/" + this.state.success_hash }</a>
                                 </div>
                               ]
                             }
@@ -169,33 +182,14 @@ class App extends React.Component {
                       </div>
                     </form>
                     <br/>
-                    <center><button className="btn btn-primary" onClick={this.sendFaucetToken.bind(this)}>Request 10 rSRK</button></center>
+                    <center><button className="btn btn-primary" onClick={this.sendFaucetToken.bind(this)}>Request 10 test SRK</button></center>
                     <div className="container">
                       <div className="row">
                         <div className="col-12">
                           <br/>
                           <div key={1} className="alert alert-warning">
-                            <strong>Did not receive your rSRK?</strong><br/>
+                            <strong>Did not receive your test SRK?</strong><br/>
                             EVM blockchains in general and Ropsten in particular are incredibly fragile, unreliable and hostile environments. Transactions do get lost, sometimes fail to mine, gas limits are estimated incorrectly, network explorer loses records, and Infura sometimes misreport nonces.
-                          </div>
-                        </div>
-                        <div className="col-12">
-                          <div key={1} className="alert alert-danger">
-                            <strong>Warning!</strong><br/>
-                            Ropsten is shutting down Q4 of 2022. More info <a href="https://blog.ethereum.org/2022/06/21/testnet-deprecation/" target="_blank" rel="noreferrer">here</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </Tab>
-                  <Tab eventKey="second" title="ETH Goerli">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-12">
-                          <br/>
-                          <div key={1} className="alert alert-info">
-                            <strong>Coming Soon!</strong><br/>
-                            This feature is still under development.
                           </div>
                         </div>
                       </div>
